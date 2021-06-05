5ef33841f980dc0ed19a9ff19716488f762f818b
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2021-06-05 19:04:22 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 31 20:20:23 2021
+
+@author: Admin
+"""
+import sys
+sys.path.insert(0,"D:/MHS detection")
+import config
+import torch
+import torch.optim as optim
+from tqdm import tqdm
+from model import YOLOv3
+from utils import (
+    mean_average_precision,
+    cells_to_bboxes,
+    get_evaluation_bboxes,
+    save_checkpoint,
+    load_checkpoint,
+    check_class_accuracy,
+    get_loaders,
+    plot_couple_examples
+)
+from loss import YoloLoss
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+if __name__ == "__main__":
+
+    torch.backends.cudnn.benchmark = True
+    
+    
+    model     = YOLOv3(num_classes=config.NUM_CLASSES).to(config.DEVICE)
+    optimizer = optim.Adam(model.parameters(), lr=config.LEARNING_RATE, weight_decay=config.WEIGHT_DECAY)
+    loss_fn   = YoloLoss()
+    scaler    = torch.cuda.amp.GradScaler()
+    
+    train_loader, test_loader = get_loaders("D:/MHS data box labeling/data/l_train.npy", "D:/MHS data box labeling/data/test.npy")
+    
+    if config.LOAD_MODEL:
+        load_checkpoint(config.CHECKPOINT_FILE, model, optimizer, config.LEARNING_RATE)
+    
+    scaled_anchors = (torch.tensor(config.ANCHORS)* torch.tensor(config.S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)).to(config.DEVICE)
+    best_mapv = 0 
+    for epoch in range(1,config.NUM_EPOCHS+1):
+        #plot_couple_examples(model, test_loader, 0.6, 0.5, scaled_anchors)
+    
+        ################################################################## Train one epoch ############################################################
+        loop   = tqdm(train_loader, leave=True)
+        losses = []
+        for batch_idx, (x, y) in enumerate(loop):
+            x = x.float()
+            x = x.to(config.DEVICE)
+            y0, y1, y2 = (y[0].to(config.DEVICE),y[1].to(config.DEVICE),y[2].to(config.DEVICE),)
+    
+            with torch.cuda.amp.autocast():
+                out  = model(x)
+                loss = (loss_fn(out[0], y0, scaled_anchors[0])+ loss_fn(out[1], y1, scaled_anchors[1])+ loss_fn(out[2], y2, scaled_anchors[2]))
+    
+            losses.append(loss.item())
+            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+    
+            # update progress bar
+            mean_loss = sum(losses) / len(losses)
+            loop.set_postfix(loss=mean_loss)
+    
+        ################################################################## Test model ############################################################
+        if epoch % 1 == 0:
+            check_class_accuracy(model, test_loader, threshold=config.CONF_THRESHOLD)
+            
+            pred_boxes, true_boxes = get_evaluation_bboxes(test_loader,model,iou_threshold=config.NMS_IOU_THRESH,anchors=config.ANCHORS,threshold=config.CONF_THRESHOLD)
+            mapval                 = mean_average_precision(pred_boxes,true_boxes,iou_threshold=config.MAP_IOU_THRESH,box_format="midpoint",num_classes=config.NUM_CLASSES)
+            print("\n########################################################################################################################################################")
+            print("MAP: {:0.4f}".format(mapval.item()))
+            print("########################################################################################################################################################")
+            
+            if mapval.item()>=best_mapv:
+                best_mapv = mapval.item()
+                torch.save(model,'models/model.pth')
+                
+            plot_couple_examples(model, test_loader, 0.6, 0.5, scaled_anchors)
+            
+            model.train()
